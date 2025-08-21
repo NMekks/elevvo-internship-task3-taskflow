a7705504cc4ce4f3fd3b99c636d392b25a7157ad
NMekks <nusaibamekka@gmail.com>
2025-08-22 02:42:25 +0300

    added a placeholder image to the public folder and to one of the cards to test it

--- src/TaskFlow.js
+++ src/TaskFlow.js
@@ -62,6 +62,49 @@ const TaskFlow = () => {
         </div>
       </div>
 
+      <div className="quotes">
+        <div class="max-w-4xl bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden m-auto mb-10">
+          <div class="p-6">
+            <img
+                    src="%_"
+                    alt="Avatar"
+                    style={{ width: 80 }}
+                />
+            <h5 class="text-xl font-bold mb-2">Card Title</h5>
+            <p class="text-gray-700 mb-4">
+              This is a simple card built with Tailwind CSS. You can use it to
+              display content in a clean way. This is a simple card built with
+              Tailwind CSS. You can use it to display content in a clean way.
+            </p>
+          </div>
+        </div>
+
+        <div class="max-w-4xl bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden m-auto mb-10">
+          <div class="p-6">
+            
+            <h5 class="text-xl font-bold mb-2">Card Title</h5>
+            <p class="text-gray-700 mb-4">
+              This is a simple card built with Tailwind CSS. You can use it to
+              display content in a clean way. This is a simple card built with
+              Tailwind CSS. You can use it to display content in a clean way.
+            </p>
+          </div>
+        </div>
+
+        <div class="max-w-4xl bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden m-auto mb-10">
+          <div class="p-6">
+            
+            <h5 class="text-xl font-bold mb-2">Card Title</h5>
+            <p class="text-gray-700 mb-4">
+              This is a simple card built with Tailwind CSS. You can use it to
+              display content in a clean way. This is a simple card built with
+              Tailwind CSS. You can use it to display content in a clean way.
+            </p>
+          </div>
+        </div>
+        
+      </div>
+
       
     </div>
   );
