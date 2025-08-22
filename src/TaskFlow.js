10aa01dfed27c22e45537207f225278fefb7602b
NMekks <nusaibamekka@gmail.com>
2025-08-22 05:10:09 +0300

    fixed font styling for headers, finished features section

--- src/TaskFlow.js
+++ src/TaskFlow.js
@@ -4,25 +4,31 @@ const TaskFlow = () => {
   return (
     <div className="container-fluid">
       <div className="page-header">
-        <h1 className="text-amber-900">TASKFLOW</h1>
-        <p className="text-slate-600">Oragnization. Flow. Peace of mind.</p>
-        <button className="btn py-3 px-16 rounded-full border-0 bg-yellow-500 text-white text-lg">Let's Start</button>
+        <h1 className="text-amber-900">TaskFlow</h1>
+        <p className="text-slate-600 text-">
+          Oragnization. Flow. Peace of mind.
+        </p>
+        <button className="btn py-3 px-16 rounded-full border-0 bg-yellow-500 text-white text-lg">
+          Let's Start
+        </button>
       </div>
 
       <div className="cards-section">
-        <h3 className="text-center mt-48 mb-48 text-6xl text-amber-900">FEATURES</h3>
+        <h3 className="text-center mt-48 mb-48 text-6xl text-amber-900">
+          Features
+        </h3>
         <div className="cards">
           <div class="card max-w-sm bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
             <div class="p-4 justify-items-center">
-              <h5 class="text-xl font-bold mb-2">Card Title</h5>
-              <p class="text-gray-700 mb-4 mx-5">
-                This is a simple card built with Tailwind CSS. You can use it to
-                display content in a clean way. This is a simple card built with
-                Tailwind CSS. You can use it to display content in a clean way.
+              <div className="card-icon text-5xl mt-4">
+                <i class="fa-regular fa-calendar-days"></i>
+              </div>
+              <p class="text-gray-700 mx-5 text-center my-6">
+                View and manage your timelines using calendars.
               </p>
               <a
                 href="#"
-                class="block mx-auto px-4 py-2 mb-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition "
+                class="block mx-auto px-4 py-2 mb-2 text-white bg-yellow-500 rounded-lg hover:bg-amber-900 transition "
               >
                 Learn More
               </a>
@@ -31,15 +37,16 @@ const TaskFlow = () => {
 
           <div class="card max-w-sm bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
             <div class="p-4 justify-items-center">
-              <h5 class="text-xl font-bold mb-2">Card Title</h5>
-              <p class="text-gray-700 mb-4 mx-5">
-                This is a simple card built with Tailwind CSS. You can use it to
-                display content in a clean way. This is a simple card built with
-                Tailwind CSS. You can use it to display content in a clean way.
+              <div className="card-icon text-5xl mt-4">
+                <i class="fa-regular fa-pen-to-square"></i>
+              </div>
+              <p class="text-gray-700 mb-4 mx-5 text-center">
+                Add tasks and schedule your meetings automatically into your
+                calendar using Natural Language Processing.
               </p>
               <a
                 href="#"
-                class="block mx-auto px-4 py-2 mb-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition"
+                class="block mx-auto px-4 py-2 mb-2 text-white bg-yellow-500 rounded-lg hover:bg-amber-900 transition"
               >
                 Learn More
               </a>
@@ -48,15 +55,16 @@ const TaskFlow = () => {
 
           <div class="card max-w-sm bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
             <div class="p-4 justify-items-center">
-              <h5 class="text-xl font-bold mb-2">Card Title</h5>
-              <p class="text-gray-700 mb-4 mx-5">
-                This is a simple card built with Tailwind CSS. You can use it to
-                display content in a clean way. This is a simple card built with
-                Tailwind CSS. You can use it to display content in a clean way.
+              <div className="card-icon text-5xl mt-4">
+                <i class="fa-regular fa-comments"></i>
+              </div>
+              <p class="text-gray-700 mb-4 mx-5 text-center">
+                Collaborate with your team, accomplishing more tasks efficiently
+                with Team Chat and White Board.
               </p>
               <a
                 href="#"
-                class="block mx-auto mb-2 px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition"
+                class="block mx-auto mb-2 px-4 py-2 text-white bg-yellow-500 rounded-lg hover:bg-amber-900 transition"
               >
                 Learn More
               </a>
@@ -66,12 +74,15 @@ const TaskFlow = () => {
       </div>
 
       <div className="reviews">
-        <h3 className="text-center mt-48 mb-20 text-6xl text-amber-900">REVIEWS</h3>
+        <h3 className="text-center mt-48 mb-20 text-6xl text-amber-900">
+          Reviews
+        </h3>
         <div class="max-w-4xl bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden m-auto mb-10">
           <div class="p-4">
             <h5 class="text-xl font-bold mb-2 mt-1 text-amber-900">John Doe</h5>
             <p class="text-gray-700 mb-4">
-              Organization is important to me, without it my life is a mess. Taskflow helped me witht that.
+              Organization is important to me, without it my life is a mess.
+              Taskflow helped me witht that.
             </p>
           </div>
         </div>
@@ -80,7 +91,8 @@ const TaskFlow = () => {
           <div class="p-4">
             <h5 class="text-xl font-bold mb-2 mt-1 text-amber-900">Jack Doe</h5>
             <p class="text-gray-700 mb-4">
-              I've never encountered a task organization application with a good price such as TaskFlow in all my years of living.
+              I've never encountered a task organization application with a good
+              price such as TaskFlow in all my years of living.
             </p>
           </div>
         </div>
@@ -89,15 +101,18 @@ const TaskFlow = () => {
           <div class="p-4">
             <h5 class="text-xl font-bold mb-2 mt-1 text-amber-900">Jane Doe</h5>
             <p class="text-gray-700 mb-4">
-              TaskFlow was a game changer for me. I am now more organized than ever thanks to it.
+              TaskFlow was a game changer for me. I am now more organized than
+              ever thanks to it.
             </p>
           </div>
         </div>
       </div>
 
       <div className="payment-section">
-        <h3 className="text-center mt-48 mb-40 text-6xl text-amber-900">PAYMENT OPTIONS</h3>
-        <div className="payment-options mb-20">
+        <h3 className="text-center mt-48 mb-20 text-6xl text-amber-900">
+          Pricing Options
+        </h3>
+        <div className="payment-options mb-40">
           <div class="card max-w-sm h-96 bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
             <div class=" p-4 justify-items-center mx-6">
               <h5 class="text-3xl font-bold mb-0 mt-3">FREE</h5>
