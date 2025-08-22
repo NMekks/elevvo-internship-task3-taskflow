f1ae820caa5523563427250788be2e7cd9a6ae67
NMekks <nusaibamekka@gmail.com>
2025-08-22 03:35:28 +0300

    added footer and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TaskFlow from './TaskFlow';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <TaskFlow />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div id="rightfooter">
+        <h3>Follow me on</h3>
+        <div id="social-media-footer">
+          <ul>
+            <li>
+              <a href="#">
+                <i className="fa-brands fa-instagram" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa-brands fa-tiktok" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa-brands fa-x-twitter" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa-brands fa-bluesky" />
+              </a>
+            </li>
+            <li>
+              <a href="#">Cara</a>
+            </li>
+          </ul>
+        </div>
+        <br />
+        <p>© Nusaibah Mekkaoui, All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
